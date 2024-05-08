1af8ef5bc2d00879124353695049155377d54046
GSmithApps <14.gsmith.14@gmail.com>
2024-05-07 23:59:16 -0500

    good

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,9 @@
+// each line can be of 3 types: code, text, blank
+// enum lineType {
+//     code,
+//     text,
+//     blank,
+// }
 
 export function renderContent(input_text: string, language: string) {
 
@@ -27,7 +33,7 @@ export function renderContent(input_text: string, language: string) {
 
 			const indentation_blank_string = `<pre style="margin: 0px; padding: 0px; ${colorDebugStyle}"><code style="margin: 0px; padding: 0px; background-color: var(--vscode-editor-background);display: inline;" class="language-${language}">${' '.repeat(spaces)}</code></pre>`;
             
-            if (trimmedLine.startsWith('$')) {
+            if (isTrimmedLineCode(trimmedLine)) {
 				
 				const codeColorDebugStyle = colordebug ? 'background-color: lightcoral;' : '';
 	
@@ -41,20 +47,45 @@ export function renderContent(input_text: string, language: string) {
 				
 				const textDebugStyle = colordebug ? 'background-color: lightblue;' : '';
                 
-				if (i === 0 || !isTrimmedLineNaturalLanguage(lines[i - 1].trim())) {
+                // add on the text depending on the previous line. 
+                //
+                // If there is no previous line (i.e. the first line), then treat
+                // the line like the previous was code or blank -- which means to add
+                // the indentation in front.
+                //
+                // if the previous line is text, then add a space in front of the line,
+                // but not the indentation.
+                //
+                // Also, as a note, in here we get kind of lucky that if we keep adding
+                // spans, and if they overflow the line, they will automatically wrap
+                // to the next line but keep the indentation. NOPE - THIS IS A BUG
+				if (
+                    i === 0 ||
+                    isTrimmedLineCode(lines[i - 1].trim()) ||
+                    isTrimmedLineBlank(lines[i - 1].trim())
+                ) {
 					
+                    // either we are in the first line, or the previous line is code or blank.
+                    // which means the previous line is not text.
 					html_string += indentation_blank_string + `<span style="${textDebugStyle}">` + trimmedLine + '</span>';
 					
 				} else {
 
+                    // previous line does not start with $ and is not totally blank.
+                    // which means the previous line is text -- it is not code or blank.
 					html_string += `<span style="${textDebugStyle}">` + " " + trimmedLine + '</span>';
 
 				}
 				
+                // break if this is the last line.
 				if (i === lines.length - 1) {break;}
 				
+                // if the next line is code or blank, then add a break.
 				const nextlineTrimmed = lines[i + 1].trim();
-				if (!isTrimmedLineNaturalLanguage(nextlineTrimmed)) {html_string += '<br>';}
+				if (
+                    isTrimmedLineCode(nextlineTrimmed) ||
+                    isTrimmedLineBlank(nextlineTrimmed)
+                ) {html_string += '<br>';}
                 
             }
 
@@ -66,7 +97,10 @@ export function renderContent(input_text: string, language: string) {
 
 }
 
-// natural language line if not blank and not starting with a $ sign
-function isTrimmedLineNaturalLanguage(line: string) {
-	return line !== '' && !line.startsWith('$');
+function isTrimmedLineCode(line: string) {
+    return line.startsWith('$');
+}
+
+function isTrimmedLineBlank(line: string) {
+    return line === '';
 }
