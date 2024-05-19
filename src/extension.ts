f6fcd8ecb1dfb1d3e296d079dbfe6047a508ef0e
GSmithApps <14.gsmith.14@gmail.com>
2024-05-18 23:36:01 -0500

    good

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,10 @@
 import * as vscode from 'vscode';
-import { getHighlighter } from 'shiki';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('rustmd.start', async () => {
 
-			// `getHighlighter` is async, it initializes the internal and
-			// loads the themes and languages specified.
-			const highlighter = await getHighlighter({
-				themes: ['nord'],
-				langs: ['javascript'],
-			});
+			const shiki = await import('shiki');
 		
 		})
 	);
